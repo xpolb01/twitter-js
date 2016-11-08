308d4b89693216b33011fb471f3a36e439d4fe4e
Jenny Chan <jrc337@cornell.edu>
2016-11-08 13:03:01 -0500

    up to add a single-user route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  console.log("list: ", list[0]);
+  console.log(res.render( 'index', { list: list } ));
+});
+
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
